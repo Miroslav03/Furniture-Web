1d89680aa748cf795276973fbc30ade5c05d3b1c
Miroslav Ivanov <123780218+Miroslav03@users.noreply.github.com>
2025-01-30 20:32:00 +0200

    Fix: AboutSection

--- furniture-web/src/components/Home/AboutSection.jsx
+++ furniture-web/src/components/Home/AboutSection.jsx
@@ -1,22 +1,26 @@
 export const AboutSection = () => {
     return (
-        <div className="bg-white pb-24 pt-12 px-6 md:px-12 lg:px-24">
-            <div className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center gap-12">
+        <div className="bg-white pb-24 sm:pb-32 pt-0 sm:pt-12 px-6 md:px-12 lg:px-24">
+            <div className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center gap-16">
                 {/* Image Gallery */}
                 <div className="lg:w-1/2 relative">
+                    {/* Main image (always visible) */}
                     <div className="relative aspect-square bg-gray-100 rounded-xl overflow-hidden">
-                        {/* Main Image */}
                         <div className="absolute inset-0 bg-[url('/images/gallery/about-image-1.jpg')] bg-cover bg-center">
                             <div className="absolute inset-0 bg-gradient-to-t from-black/30 to-transparent rounded-lg transition-opacity duration-300 opacity-100 group-hover:opacity-0"></div>
                         </div>
+                    </div>
 
-                        {/* Decorative Element */}
+                    {/* Decorative Element (only on lg and above) */}
+                    <div className="hidden lg:block">
                         <div className="absolute -bottom-8 -right-8 w-64 h-64 bg-[url('/images/gallery/about-image-3.jpg')] rounded-xl border-8 border-white bg-cover bg-center"></div>
                     </div>
 
-                    {/* Secondary Image */}
-                    <div className="absolute -left-8 top-24 w-48 h-48 bg-gray-200 rounded-xl overflow-hidden border-4 border-white shadow-lg">
-                        <div className="bg-[url('/images/gallery/about-3.jpg')] bg-cover bg-center w-full h-full"></div>
+                    {/* Secondary Image (only on lg and above) */}
+                    <div className="hidden lg:block">
+                        <div className="absolute -left-8 top-24 w-48 h-48 bg-gray-200 rounded-xl overflow-hidden border-4 border-white shadow-lg">
+                            <div className="bg-[url('/images/gallery/about-3.jpg')] bg-cover bg-center w-full h-full"></div>
+                        </div>
                     </div>
                 </div>
 
