5f9dcc57118fc9904b55139daa528a81bf5dbd93
Miroslav Ivanov <123780218+Miroslav03@users.noreply.github.com>
2025-02-01 13:02:21 +0200

    add sitemap and robots.txt

---
+++ furniture-web/src/app/sitemap.js
@@ -0,0 +1,30 @@
+export async function GET() {
+    const pages = [
+        { url: "https://www.raevdesign.com/", lastModified: "2025-02-01" },
+        { url: "https://www.raevdesign.com/za-nas", lastModified: "2025-02-01" },
+        { url: "https://www.raevdesign.com/kontakti", lastModified: "2025-02-01" },
+        { url: "https://www.raevdesign.com/galeria", lastModified: "20254-02-01" }
+    ];
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${pages
+            .map(
+                (page) => `
+        <url>
+            <loc>${page.url}</loc>
+            <lastmod>${page.lastModified}</lastmod>
+            <changefreq>monthly</changefreq>
+            <priority>0.8</priority>
+        </url>
+        `
+            )
+            .join("")}
+    </urlset>`;
+
+    return new Response(sitemap, {
+        headers: {
+            "Content-Type": "application/xml"
+        }
+    });
+}
